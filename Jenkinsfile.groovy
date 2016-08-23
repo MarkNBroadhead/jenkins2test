b21603c38bba2b184b397849fbc9b3d659790749
Mark N Broadhead <mbroadhead@ancestry.com>
2016-08-23 13:48:33 -0600

    initial commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,31 @@
+def pipeline() {
+	node {
+		stage 'Build'
+
+		stage 'Test'
+
+		stage 'Deploy to Dev'
+
+		stage 'Test Dev'
+
+		stage 'Deploy to Stage'
+
+		stage 'Test Stage'
+
+		stage 'Deploy to Canary'
+
+		stage 'Test Canary'
+
+		stage 'Deploy to Production'
+
+		stage 'Test Production'
+
+	}
+
+}
+
+def deploy() {
+
+}
+
+this.pipeline()
